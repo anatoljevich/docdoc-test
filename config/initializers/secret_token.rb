d658f96299c607fe8ef0d464b8ef959dbbcbb61a
alexey <anatoljevich@gmail.com>
2013-07-04 14:45:36 +0300

    Create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DocdocTest::Application.config.secret_token = '902320985abdbeb0955fb6fdf53ffdadbd97a656c3df5fbaa2a25bcadeb7c8106bc6f43c3b2af5fd5f0f6ceb6aedffde7d5284c3d2fe26c21ca2694000025d2a'
